f0bc59f28cd94b9ef20839791f838e7c9ae59918
EmilianoTCS <emi.vega84@gmail.com>
2022-11-24 12:39:37 -0300

    Modificación InsertarCursos con Multi DatePicker finalizada

--- TASKS/coe-insertarCurso.php
+++ TASKS/coe-insertarCurso.php
@@ -11,38 +11,55 @@ include("../security/logBuilder.php");
 
 if (isset($_GET['insertarCurso'])) {
     $data = json_decode(file_get_contents("php://input"));
+    $duracion = $data->duracion;
     $codigoCuenta = $data->codigoCuenta;
     $codigoRamo = $data->codigoRamo;
-    $fechaInicio = $data->fechaInicio;
-    $fechaFin = $data->fechaFin;
-    $horaInicio = $data->horaInicio;
-    $horaFin = $data->horaFin;
+    $fechas = $data->fechasFormateadas;
+    $longitud = count($fechas);
+    $primerElemento = reset($fechas);
+    $ultimoElemento = end($fechas);
 
-    $dateformat_inicio = date("Y-m-d", strtotime($fechaInicio));
-    $dateformat_fin = date("Y-m-d", strtotime($fechaFin));
+    $fechaInicioTemporal = date_create($primerElemento);
+    $fechaFinalTemporal = date_create($ultimoElemento);
+
+    $fechaInicio = date_format($fechaInicioTemporal, 'Y-m-d');
+    $fechaFin = date_format($fechaFinalTemporal, 'Y-m-d');
 
 
-    $fechaInicio_mod = preg_replace('/-/', '', $dateformat_inicio);
-    $horaInicio_mod = preg_replace('/:/', '', $horaInicio);
+    for ($i = 0; $i < $longitud; ++$i) {
 
-    $codigoCurso = $codigoRamo . $fechaInicio_mod . $horaInicio_mod;
+        $createDateTemporal = date_create($fechas[$i]);
+        $formatTimeTemporal = date_format($createDateTemporal, 'H:i:s');
+        $formatDateTemporal = date_format($createDateTemporal, 'Y-m-d');
+        $fechaInicio_mod = preg_replace('/-/', '', $formatDateTemporal);
+        $horaInicio_mod = preg_replace('/:/', '', $formatTimeTemporal);
 
-    $queryVerify = "SELECT * FROM cursos WHERE codigoRamo = '$codigoRamo' AND inicio = '$dateformat_inicio' AND fin = '$dateformat_fin' AND hora_inicio <= time('$horaInicio') AND hora_fin >= time('$horaFin') ";
-    $resultVerify = mysqli_query($conection, $queryVerify);
+        $codigoCurso = $codigoRamo . $fechaInicio_mod . $horaInicio_mod;
+        $hora1 = strtotime($formatTimeTemporal);
+        $hora2 = strtotime($duracion);
+        $horaFin = date('H:i:s', $hora1 + $hora2);
 
-    if (mysqli_num_rows($resultVerify) >= 1) {
-        echo json_encode('errorRepeated');
-    } else {
-        $query = "INSERT INTO cursos (idCuenta, codigoCurso, codigoRamo, inicio, fin, hora_inicio, hora_fin, isActive) VALUES ('$codigoCuenta','$codigoCurso','$codigoRamo','$dateformat_inicio','$dateformat_fin','$horaInicio','$horaFin', true);";
-        $result = mysqli_query($conection, $query);
-        if (!$result) {
-            die('Query Failed' . mysqli_error($conection));
+        // echo $horaFin;
+        $queryVerify = "SELECT * FROM cursos WHERE codigoRamo = '$codigoRamo' AND inicio = '$fechaInicio' AND fin = '$fechaFin' AND hora_inicio <= time('$formatTimeTemporal') AND hora_fin >= time('$horaFin') ";
+        $resultVerify = mysqli_query($conection, $queryVerify);
+
+
+        if (mysqli_num_rows($resultVerify) >= 1) {
+            echo json_encode('errorRepeated');
         } else {
-            echo json_encode("successCreated");
-            // $usuario = $_SESSION['codigoCuenta'];
-            // $log = new Log("../security/reports/log.txt");
-            // $log->writeLine("I", " ha agregado el curso con los datos: [$codigoCuenta, $codigoCurso, $codigoRamo, $dateformat_inicio, $dateformat_fin, $horaInicio, $horaFin]");
-            // $log->close();
+
+            $query = "INSERT INTO cursos (idCuenta, codigoCurso, codigoRamo, fecha_hora, inicio, fin, hora_inicio, hora_fin, isActive) VALUES ('$codigoCuenta','$codigoCurso','$codigoRamo','$fechas[$i]','$fechaInicio', '$fechaFin', '$formatTimeTemporal', '$horaFin', true);";
+            $result = mysqli_query($conection, $query);
+            if (!$result) {
+                die('Query Failed' . mysqli_error($conection));
+            } else {
+                echo json_encode("successCreated");
+                // $usuario = $_SESSION['codigoCuenta'];
+                // $log = new Log("../security/reports/log.txt");
+                // $log->writeLine("I", " ha agregado el curso con los datos: [$codigoCuenta, $codigoCurso, $codigoRamo, $dateformat_inicio, $dateformat_fin, $horaInicio, $horaFin]");
+                // $log->close();
+
+            }
         }
     }
 } else {
