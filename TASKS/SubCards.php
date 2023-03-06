c10b70d3f3bec4eafacc43cd8b037ce27ddef2a2
EmilianoTCS <96253955+EmilianoTCS@users.noreply.github.com>
2023-03-06 10:59:06 -0300

    fix SubCards

--- TASKS/SubCards.php
+++ TASKS/SubCards.php
@@ -12,7 +12,7 @@ if (isset($_GET['usuario'])) {
     $query_total_cursos = mysqli_query($conection, "SELECT ram.nombreRamo, cur.inicio,cur.fin, cur.codigoCurso, ROUND(AVG(eva.nota),2) as promedio, eva.usuario,
 												5 * (DATEDIFF(cur.fin, cur.inicio) DIV 7) + MID('0123444401233334012222340111123400012345001234550', 7 * WEEKDAY(cur.inicio) + WEEKDAY(cur.fin) + 1, 1)AS totalClases,
 												IF(cur.fin < date(CURRENT_DATE), 'Finalizado', IF(eva.estado = 'Enviado' || 'Entregado', 'Activo', IF(eva.estado != 'Enviado' || 'Entregado', 'Pendiente', ''))) as estado
-												FROM cursos cur INNER JOIN evaluaciones eva, ramos ram WHERE cur.codigoRamo = ram.codigoRamo AND cur.isActive = true AND cur.codigoCurso = eva.codigoCurso AND eva.usuario = '$usuario' GROUP BY cur.codigoCurso ORDER BY cur.codigoRamo ASC");
+												FROM cursos cur INNER JOIN evaluaciones eva, ramos ram WHERE cur.idRamo = ram.ID AND cur.isActive = true AND cur.codigoCurso = eva.codigoCurso AND eva.usuario = '$usuario' GROUP BY cur.codigoCurso ORDER BY cur.ID ASC");
     $result = mysqli_num_rows($query_total_cursos);
     if ($result > 0) {
         while ($data = mysqli_fetch_array($query_total_cursos)) {
